a90d59898b58000e1c7181a70f7343e3a0496e11
Clancey <Clancey>
2024-01-22 23:45:03 +0000

    Pushed from Locofy Builder

--- src/pages/PropertyDetails.tsx
+++ src/pages/PropertyDetails.tsx
@@ -1,10 +1,10 @@
 import { FunctionComponent } from "react";
-import Header from "../components/Header";
+import Header from "../components/Header1";
 import Freeparking from "../components/Freeparking";
 import ListingDetailsSection from "../components/ListingDetailsSection";
 import ReviewSection from "../components/ReviewSection";
 import SimilarStaysSection from "../components/SimilarStaysSection";
-import Footer from "../components/Footer";
+import Footer from "../components/Footer1";
 import styles from "./PropertyDetails.module.css";
 
 const PropertyDetails: FunctionComponent = () => {
